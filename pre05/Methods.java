0a743e62410fd6480d399b56a7097cfc14675e99
herrkm <96203602+herrkm@users.noreply.github.com>
2022-01-13 17:39:45 +0000

    completed prework 5 in Methods.java

---
+++ pre05/Methods.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Methods {
+
+  public static void main(String args[]){}
+  
+  /* exercise 2 */
+  public static boolean isDivisible(int n, int m) {
+    if (m == 0) {
+      System.out.println("Cannot divide by zero");
+      return false;
+    } else {
+      return n % m == 0;
+    }
+  }
+
+  /* exercise 3 */
+  public static boolean isTriangle(int a, int b, int c){
+    if (a == Math.max(Math.max(a,b),c)){
+      return b + c > a;
+    } else if (b == Math.max(Math.max(a,b),c)){
+      return a + c > b;
+    } else {
+      return a + b > c;
+    }
+  }
+
+  /* exercise 8 */
+  public static int ack(int m, int n){
+    if (m == 0){
+      return n + 1;
+    } else if (n == 0){
+      return ack(m - 1, 1);
+    } else {
+      return ack(m - 1, ack(m, n-1));
+    }
+  }
+}
