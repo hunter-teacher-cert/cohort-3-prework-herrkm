adfb51b7d0940c6a32f50a668e05e6ab02e3ae78
herrkm <96203602+herrkm@users.noreply.github.com>
2022-01-19 13:24:25 +0000

    Finished Craps.java; completed round() method and added code in main() to run round() a number of times input in the command line.

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -4,10 +4,13 @@ import java.util.*;
 public class Craps{
   
   public static void main(String args[]){
-    shoot(2, 6);
-    shoot(5, 6);
-    shoot(1, 20);
-    shoot(5, 4);
+    for (String arg : args){
+      int nRounds = Integer.parseInt(arg);
+      for (int i = 1; i <= nRounds; i++){
+        round();
+        System.out.println();
+      }
+    }
   }
   
   public static int roll(int maxRoll){
@@ -25,7 +28,8 @@ public class Craps{
   }
 
   public static boolean round(){ //return true if win, false if lose
-    int roll = shoot(2, 6);
+    int nDice = 2, maxRoll = 6;
+    int roll = shoot(nDice, maxRoll);
     System.out.println("You rolled: " + roll);
     if (roll == 2 || roll == 3 || roll == 12){
       System.out.println("Craps! You lose.");
@@ -35,8 +39,18 @@ public class Craps{
       return true;
     } else {
       int point = roll;
+      System.out.println(roll + " is now your point value.");
+      do {
+        roll = shoot(nDice, maxRoll);
+        System.out.println("You rolled: " + roll);
+      } while (roll != point && roll != 7);
+      if (roll == point){
+        System.out.println("You rolled the point value, " + point + ". You win!");
+        return true;
+      } else {
+        System.out.println("You rolled a 7. You lose.");
+        return false;
+      }
     }
-
-    return true;
   }
 }
